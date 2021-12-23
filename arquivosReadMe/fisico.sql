34531e13978ffa90860ba9cebe86393dbb4de5b6
João Pedro de Oliveira Nascente <joaopedro_nascente@outlook.com>
2021-12-22 22:51:01 -0300

    novo modelo fisico

--- arquivosReadMe/fisico.sql
+++ arquivosReadMe/fisico.sql
@@ -1,53 +1,76 @@
--- Geração de Modelo físico
--- Sql ANSI 2003 - brModelo.
+-- Modelo físico/Script de geração
+-- Schema mydb
+DROP SCHEMA IF EXISTS mydb CASCADE;
+
+CREATE SCHEMA IF NOT EXISTS mydb;
+SET SCHEMA 'mydb' ;
+
+DROP TABLE IF EXISTS mydb.usuario ;
+
+CREATE TABLE IF NOT EXISTS mydb.usuario (
+  idUsuario INT NOT NULL,
+  dscNomeUsuario VARCHAR(50) NOT NULL,
+  dscEmailUsuario VARCHAR(50) NOT NULL,
+  dscSenhaUsuario VARCHAR(15) NOT NULL,
+  dscIdentificacaoUsuario VARCHAR(15) NOT NULL,
+  dscUsuario VARCHAR(500) NULL,
+  PRIMARY KEY (idUsuario)
+ );
 
 
+DROP TABLE IF EXISTS mydb.tpVeiculo ;
 
-CREATE TABLE contato (
-dscContatoLoja VARCHAR(10),
-idTpContato VARCHAR(10),
-idPessoa VARCHAR(10)
-)
+CREATE TABLE IF NOT EXISTS mydb.tpVeiculo (
+  idTpVeiculo INT NOT NULL,
+  dscTpVeiculo VARCHAR(50) NOT NULL,
+  PRIMARY KEY (idTpVeiculo)
+);
 
-CREATE TABLE tpContato (
-idTpContato VARCHAR(10) PRIMARY KEY,
-dscTpContato VARCHAR(10)
-)
 
-CREATE TABLE Produto (
-idProduto VARCHAR(10) PRIMARY KEY,
-tpProduto VARCHAR(10),
-dscProduto VARCHAR(10)
-)
+DROP TABLE IF EXISTS mydb.veiculo;
+CREATE TABLE IF NOT EXISTS mydb.veiculo (
+  idVeiculo INT NOT NULL,
+  dscVeiculo VARCHAR(50) NOT NULL,
+  usuario_idUsuario INT NOT NULL,
+  tpVeiculo_idTpVeiculo INT NOT NULL,
+  PRIMARY KEY (idVeiculo),
+  FOREIGN KEY (usuario_idUsuario)
+    REFERENCES mydb.usuario (idUsuario),
+  FOREIGN KEY (tpVeiculo_idTpVeiculo)
+    REFERENCES mydb.tpVeiculo (idTpVeiculo)
+);
 
-CREATE TABLE catalogo (
-idCatalogo VARCHAR(10) PRIMARY KEY,
-dscPrecoCatalogo VARCHAR(10),
-idProduto VARCHAR(10),
-idPessoa VARCHAR(10),
-FOREIGN KEY(idProduto) REFERENCES Produto (idProduto)
-)
 
-CREATE TABLE Pessoa (
-dscNomPessoa VARCHAR(10),
-idPessoa VARCHAR(10) PRIMARY KEY,
-endereco VARCHAR(10)
-)
+drop table if exists mydb.endereco;
 
-CREATE TABLE pedido (
-idPedido VARCHAR(10) PRIMARY KEY,
-idPessoa VARCHAR(10),
-FOREIGN KEY(idPessoa) REFERENCES Pessoa (idPessoa)
-)
+create table if not exists mydb.endereco (
+	idEndereco INT not null,
+	dscEndereco VARCHAR(50) not null,
+	dscCEPEndereco VARCHAR(8) not null,
+	dscNumEndereco VARCHAR(10) not null,
+	dscBairro VARCHAR(50) NOT NULL,
+	dscCidade VARCHAR(50) NOT NULL,
+	primary key (idEndereco)
+);
 
-CREATE TABLE itensVendidos (
-dscQtd VARCHAR(10),
-idCatalogo VARCHAR(10),
-idPedido VARCHAR(10),
-FOREIGN KEY(idCatalogo) REFERENCES catalogo (idCatalogo),
-FOREIGN KEY(idPedido) REFERENCES pedido (idPedido)
-)
 
-ALTER TABLE contato ADD FOREIGN KEY(idTpContato) REFERENCES tpContato (idTpContato)
-ALTER TABLE contato ADD FOREIGN KEY(idPessoa) REFERENCES Pessoa (idPessoa)
-ALTER TABLE catalogo ADD FOREIGN KEY(idPessoa) REFERENCES Pessoa (idPessoa)
+drop table if exists mydb.pedido;
+
+create table if not exists mydb.pedido(
+	idPedido INT not null,
+	dscDataPedido DATE NOT NULL,
+	dscDataMaxPedido DATE NOT NULL,
+	usuario_idEntregador INT NOT NULL,
+	usuario_idCliente INT NOT NULL,
+	endereco_idEnderecoRetirada INT NOT NULL,
+	endereco_idEnderecoEntrega INT NOT NULL,
+	primary key (idPedido),
+	foreign key (usuario_idEntregador)
+		references mydb.usuario (idUsuario),
+	FOREIGN KEY (usuario_idCliente)
+		REFERENCES mydb.usuario (idUsuario),
+	FOREIGN KEY (endereco_idEnderecoRetirada)
+		REFERENCES mydb.endereco (idEndereco),
+	FOREIGN KEY (endereco_idEnderecoEntrega)
+		REFERENCES mydb.endereco (idEndereco)
+);
