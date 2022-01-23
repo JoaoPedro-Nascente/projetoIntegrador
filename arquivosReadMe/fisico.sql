ae392fe8e7d773868a1fc52272a20b7d166f7e3a
João Pedro de Oliveira Nascente <joaopedro_nascente@outlook.com>
2022-01-23 19:17:06 -0300

    Atualizacao modelos do bd

--- arquivosReadMe/fisico.sql
+++ arquivosReadMe/fisico.sql
@@ -1,76 +1,30 @@
--- Modelo físico/Script de geração
--- Schema mydb
-DROP SCHEMA IF EXISTS mydb CASCADE;
-
-CREATE SCHEMA IF NOT EXISTS mydb;
-SET SCHEMA 'mydb' ;
-
-DROP TABLE IF EXISTS mydb.usuario ;
-
-CREATE TABLE IF NOT EXISTS mydb.usuario (
-  idUsuario INT NOT NULL,
-  dscNomeUsuario VARCHAR(50) NOT NULL,
-  dscEmailUsuario VARCHAR(50) NOT NULL,
-  dscSenhaUsuario VARCHAR(15) NOT NULL,
-  dscIdentificacaoUsuario VARCHAR(20) NOT NULL,
-  dscUsuario VARCHAR(500) NULL,
-  PRIMARY KEY (idUsuario)
- );
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
 
 
-DROP TABLE IF EXISTS mydb.tpVeiculo ;
 
-CREATE TABLE IF NOT EXISTS mydb.tpVeiculo (
-  idTpVeiculo INT NOT NULL,
-  dscTpVeiculo VARCHAR(50) NOT NULL,
-  PRIMARY KEY (idTpVeiculo)
-);
+CREATE TABLE categoriaVeiculo (
+dscCategoriaVeiculo VARCHAR(10),
+idCategoriaVeiculo VARCHAR(10) PRIMARY KEY
+)
 
+CREATE TABLE veiculo (
+dscVeiculo VARCHAR(10),
+idVeiculo VARCHAR(10) PRIMARY KEY,
+idCategoriaVeiculo VARCHAR(10),
+idUsuario VARCHAR(10),
+FOREIGN KEY(idCategoriaVeiculo) REFERENCES categoriaVeiculo (idCategoriaVeiculo)
+)
 
-DROP TABLE IF EXISTS mydb.veiculo;
-CREATE TABLE IF NOT EXISTS mydb.veiculo (
-  idVeiculo INT NOT NULL,
-  dscVeiculo VARCHAR(50) NOT NULL,
-  usuario_idUsuario INT NOT NULL,
-  tpVeiculo_idTpVeiculo INT NOT NULL,
-  PRIMARY KEY (idVeiculo),
-  FOREIGN KEY (usuario_idUsuario)
-    REFERENCES mydb.usuario (idUsuario),
-  FOREIGN KEY (tpVeiculo_idTpVeiculo)
-    REFERENCES mydb.tpVeiculo (idTpVeiculo)
-);
+CREATE TABLE usuario (
+dscIdentificacaoUsuario VARCHAR(10),
+dscNomeUsuario VARCHAR(10),
+dscEmailUsuario VARCHAR(10),
+dscSenhaUsuario VARCHAR(10),
+dscUsuario VARCHAR(10),
+idUsuario VARCHAR(10) PRIMARY KEY,
+dscAreaAtuacao VARCHAR(10),
+dscTelUsuario VARCHAR(10)
+)
 
-
-drop table if exists mydb.endereco;
-
-create table if not exists mydb.endereco (
-	idEndereco INT not null,
-	dscEndereco VARCHAR(50) not null,
-	dscCEPEndereco VARCHAR(8) not null,
-	dscNumEndereco VARCHAR(10) not null,
-	dscBairro VARCHAR(50) NOT NULL,
-	dscCidade VARCHAR(50) NOT NULL,
-	primary key (idEndereco)
-);
-
-
-drop table if exists mydb.pedido;
-
-create table if not exists mydb.pedido(
-	idPedido INT not null,
-	dscDataPedido DATE NOT NULL,
-	dscDataMaxPedido DATE NOT NULL,
-	usuario_idEntregador INT NOT NULL,
-	usuario_idCliente INT NOT NULL,
-	endereco_idEnderecoRetirada INT NOT NULL,
-	endereco_idEnderecoEntrega INT NOT NULL,
-	primary key (idPedido),
-	foreign key (usuario_idEntregador)
-		references mydb.usuario (idUsuario),
-	FOREIGN KEY (usuario_idCliente)
-		REFERENCES mydb.usuario (idUsuario),
-	FOREIGN KEY (endereco_idEnderecoRetirada)
-		REFERENCES mydb.endereco (idEndereco),
-	FOREIGN KEY (endereco_idEnderecoEntrega)
-		REFERENCES mydb.endereco (idEndereco)
-);
+ALTER TABLE veiculo ADD FOREIGN KEY(idUsuario) REFERENCES usuario (idUsuario)
